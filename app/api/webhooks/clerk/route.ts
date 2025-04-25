3fb5fd3a2177baeb22154903920a36a037df91cc
SudarshanDhage <sudarshan096k@gmail.com>
2025-04-25 15:56:56 +0530

    - implement clerk webhook

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,55 @@
+import { db } from "@/lib/db";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+
+    // Do something with payload
+    // For this guide, log payload to console
+    const { id } = evt.data;
+    const eventType = evt.type;
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`
+    );
+    console.log("Webhook payload:", evt.data);
+
+    if (eventType === "user.created") {
+      await db.user.create({
+        data: {
+          externalUserId: evt.data.id,
+          username: evt.data.username || `user_${evt.data.id}`,
+          imageUrl: evt.data.image_url,
+        },
+      });
+    }
+
+    if (eventType === "user.updated") {
+      await db.user.update({
+        where: {
+          externalUserId: evt.data.id,
+        },
+        data: {
+          username: evt.data.username || `user_${evt.data.id}`,
+          imageUrl: evt.data.image_url,
+        },
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      //ait resetIngresses(evt.data.id);
+
+      await db.user.delete({
+        where: {
+          externalUserId: evt.data.id,
+        },
+      });
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+// Add /api/webhooks(.*) to public routes
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
@@ -10,7 +15,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
